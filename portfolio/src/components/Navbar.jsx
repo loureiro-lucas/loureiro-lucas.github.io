ed9451602ce1ba8318a99171784a35e76f95e64c
loureiro-lucas <lucas-loureiro@outlook.com>
2021-12-21 20:29:52 -0300

    navbar in progress

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MenuItems from './MenuItems';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [isMenuShown, setIsMenuShown] = useState(false);
+
   return (
     <nav className="navbar-container">
-      <div className="menu-icon">
-        { /* ícone do menu */ }
+      <div className="menu-icon" onClick={ () => setIsMenuShown(!isMenuShown) }>
+        <i className={ isMenuShown ? 'fas fa-times' : 'fas fa-bars' }></i>
       </div>
-      <ul className='menu-items-container'>
+      <ul className={ isMenuShown ? 'menu-items-container active' : 'menu-items-container'} >
         { MenuItems.map(({ title, url, navClass }, index) => (
-          <li
-            key={ index }
-            className={ navClass }
-          >
-            <Link to={ url }>
+          <li key={ index }>
+            <Link
+              to={ url }
+              className={ navClass }
+            >
               { title }
             </Link>
           </li>
